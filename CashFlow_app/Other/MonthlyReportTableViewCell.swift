13e3b04d02f5013eaee824454721af14b004ae10
esMatthew <118026823+esMatthew@users.noreply.github.com>
2023-07-09 16:08:08 -0500

    Began implementation for the MonthlyReportViewController

--- CashFlow_app/Other/MonthlyReportTableViewCell.swift
+++ CashFlow_app/Other/MonthlyReportTableViewCell.swift
@@ -10,15 +10,43 @@ import UIKit
 class MonthlyReportTableViewCell: UITableViewCell {
     static let identifier = "MonthlyReportTableViewCellIdentifier"
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    public var categoryLabel: UILabel {
+        let label = UILabel()
+        
+        label.text          = "Test"
+        label.textAlignment = .center
+        label.font          = UIFont(name: "Avenir-Medium", size: 15)
+        label.textColor     = Colors.lightTextLabel
+        
+        return label
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    public var valueLabel: UILabel {
+        let label = UILabel()
+        
+        label.text          = "Test"
+        label.textAlignment = .center
+        label.font          = UIFont(name: "Avenir-Medium", size: 15)
+        label.textColor     = Colors.lightTextLabel
+        
+        return label
     }
-
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super .init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.addSubview(categoryLabel)
+        contentView.addSubview(valueLabel)
+        
+        applyConstraints()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError()
+    }
+    
+    private func applyConstraints() {
+        categoryLabel.frame = CGRect(x: 0, y: contentView.bounds.minY, width: contentView.bounds.width / 2, height: contentView.bounds.height)
+        valueLabel.frame = CGRect(x: contentView.bounds.width / 2, y: contentView.bounds.minY, width: contentView.bounds.width / 2, height: contentView.bounds.height)
+    }
 }
--- CashFlow_app/View Controllers/MonthlyReportViewController.swift
+++ CashFlow_app/View Controllers/MonthlyReportViewController.swift
@@ -11,6 +11,9 @@ class MonthlyReportViewController: UIViewController {
     private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     var categories: [CategoryTableViewItem] = [CategoryTableViewItem]()
+    var models    : [TableViewItem]         = [TableViewItem]()
+    
+    let monthNames: [String] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "Dicember"]
     
     let tableView: UITableView = {
         let tableView = UITableView()
@@ -25,6 +28,9 @@ class MonthlyReportViewController: UIViewController {
         
         view.backgroundColor = Colors.lightBackground
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         view.addSubview(tableView)
         
         getAllItems()
@@ -37,6 +43,7 @@ class MonthlyReportViewController: UIViewController {
     
     func getAllItems() {
         do {
+            models     = try context.fetch(TableViewItem.fetchRequest())
             categories = try context.fetch(CategoryTableViewItem.fetchRequest())
             
             DispatchQueue.main.async {
@@ -55,13 +62,32 @@ extension MonthlyReportViewController: UITableViewDataSource, UITableViewDelegat
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return categories.count + 1
+        return categories.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return monthNames[section]
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // First cell returns a comparison between the total initial amount of money in the month, and the end of it
         // Next cells return the amount of money spent in a certain category
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MonthlyReportTableViewCell.identifier) as? MonthlyReportTableViewCell else { return UITableViewCell() }
         
-        return UITableViewCell()
+        cell.categoryLabel.text = "Category"
+        cell.valueLabel.text = "Value"
+        
+        if(traitCollection.userInterfaceStyle == .dark) {
+            cell.backgroundColor         = Colors.darkTableView
+            cell.categoryLabel.textColor = Colors.darkTextLabel
+            cell.valueLabel.textColor    = Colors.darkTextLabel
+        }
+        else {
+            cell.backgroundColor         = Colors.lightTableView
+            cell.categoryLabel.textColor = Colors.lightTextLabel
+            cell.valueLabel.textColor    = Colors.lightTextLabel
+        }
+        
+        return cell
     }
 }
