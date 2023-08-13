8a95e8b1f338272fe69d2faed6e8876a68217eda
esMatthew <118026823+esMatthew@users.noreply.github.com>
2023-08-12 21:05:02 -0500

    Finished Monthly Report primary functionality
    Now the page shows the amount of money spent per category per month, depending on the dates in which the models were inputted

--- CashFlow_app/View Controllers/MonthlyReportViewController.swift
+++ CashFlow_app/View Controllers/MonthlyReportViewController.swift
@@ -4,17 +4,21 @@
 //
 //  Created by Mate Escobar on 8/07/23.
 //
-
+import Foundation
 import UIKit
 
 // TODO: Fill the sections BEFORE the current month by saving the monthly reports at the end of each month.
 
 class MonthlyReportViewController: UIViewController {
+    
     private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     private var models: [TableViewItem] = [TableViewItem]()
+    private var categories: [CategoryTableViewItem] = [CategoryTableViewItem]()
     
     private var modelsPerMonth: [[TableViewItem]] = [[TableViewItem]]()
+    private var categoryStrings: [String] = [String]()
+    private var valuePerCategoryPerMonth: [[Int]] = [[Int]]()
     
     let tableView: UITableView = {
         let tableView = UITableView()
@@ -30,7 +34,10 @@ class MonthlyReportViewController: UIViewController {
         view.backgroundColor = Colors.lightBackground
         
         getAllItems()
+        
         sortByMonth()
+        getCategories()
+        sortByCategory()
         
         tableView.delegate   = self
         tableView.dataSource = self
@@ -46,6 +53,7 @@ class MonthlyReportViewController: UIViewController {
     func getAllItems() {
         do {
             models = try context.fetch(TableViewItem.fetchRequest())
+            categories = try context.fetch(CategoryTableViewItem.fetchRequest())
             
             DispatchQueue.main.async {
                 self.tableView.reloadData()
@@ -58,17 +66,53 @@ class MonthlyReportViewController: UIViewController {
     
     private func sortByMonth() {
         let currentMonth = Calendar.current.component(.month, from: Date())
-        
-        for _ in 0...currentMonth - 1 {
+    
+        for i in 0...currentMonth - 1  {
             modelsPerMonth.append([TableViewItem]())
-        }
-        
-        for i in 0...modelsPerMonth.count - 1 {
             for j in 0...models.count - 1 {
                 let modelDate = Calendar.current.component(.month, from: models[j].date ?? Date())
                 
                 if modelDate == i + 1 {
                     modelsPerMonth[i].append(models[j])
+                }
+            }
+        }
+    }
+    
+    private func getCategories() {
+        for i in 0...categories.count - 1 {
+            categoryStrings.append(categories[i].transferType ?? "Unknown")
+        }
+        
+        categoryStrings.insert("Initial Balance", at: 0)
+        categoryStrings.insert("Income", at: 0)
+    }
+    
+    private func sortByCategory() {
+        var sum = 0
+        var buffer = 0
+        
+        // print("Month    Category    Model   Sum")
+        
+        for month in 0...modelsPerMonth.count - 1 {
+            valuePerCategoryPerMonth.append([Int]())
+            
+            if(modelsPerMonth[month].count != 0) {
+                for category in 0...categoryStrings.count - 1 {
+                    for model in 0...modelsPerMonth[month].count - 1 {
+                        // print("\(month)        \(category)           \(model)       \(sum)")
+                        
+                        if(buffer != category) {
+                            buffer = category
+                            sum = 0
+                        }
+                        
+                        if(modelsPerMonth[month][model].transferType == categoryStrings[category]) {
+                            sum += Int(modelsPerMonth[month][model].value ?? "0") ?? 0
+                        }
+                    }
+                    
+                    valuePerCategoryPerMonth[month].append(sum)
                 }
             }
         }
@@ -92,11 +136,11 @@ extension MonthlyReportViewController: UITableViewDataSource, UITableViewDelegat
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return valuePerCategoryPerMonth[section].count
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return  Calendar.current.monthSymbols[section]
+        return Calendar.current.monthSymbols[section]
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -104,8 +148,18 @@ extension MonthlyReportViewController: UITableViewDataSource, UITableViewDelegat
         
         guard let cell = tableView.dequeueReusableCell(withIdentifier: MonthlyReportTableViewCell.identifier) as? MonthlyReportTableViewCell else { return UITableViewCell() }
         
-        cell.valueLabel.text = "Test"
-        cell.categoryLabel.text = "Test"
+        let model = valuePerCategoryPerMonth[indexPath.section][indexPath.row]
+        let category = categoryStrings[indexPath.row]
+        
+        if(category == "Initial Balance" || category == "Income") {
+            cell.valueLabel.textColor = .systemGreen
+        }
+        else {
+            cell.valueLabel.textColor = .systemRed
+        }
+        
+        cell.valueLabel.text = formatNumber(number: model)
+        cell.categoryLabel.text = category
         
         return cell
     }
